51f82bb5d5658190a6473fe7318dd14aa55c1b55
SirSalamandra <zxjoaozx@gmail.com>
2022-03-17 13:52:05 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubWorkFlowExemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
